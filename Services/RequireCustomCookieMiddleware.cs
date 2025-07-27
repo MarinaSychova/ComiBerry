b28ecaeb28610fb933e30028ff35d2282ccae0d4
MarinaSychova <139264690+MarinaSychova@users.noreply.github.com>
2025-07-27 12:41:28 +0300

    Add files via upload

---
+++ Services/RequireCustomCookieMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace ComiBerry.Services
+{
+    public class RequireCustomCookieMiddleware(RequestDelegate next)
+    {
+        private readonly RequestDelegate _next = next;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var path = context.Request.Path.Value;
+
+            if (context.User.Identity!.IsAuthenticated && path is not null && !path!.StartsWith("/Navigation")
+                && (!context.Request.Cookies.ContainsKey("UserId") || !context.Request.Cookies.ContainsKey("UserName") || !context.Request.Cookies.ContainsKey("UserEmail")))
+            {
+                await context.SignOutAsync(IdentityConstants.ApplicationScheme);
+                context.Response.Redirect("/Navigation/Home");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
---
+++ Services/RequireValidUserMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace ComiBerry.Services
+{
+    public class RequireValidUserMiddleware(RequestDelegate next)
+    {
+        private readonly RequestDelegate _next = next;
+
+        public async Task InvokeAsync(HttpContext context, UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            if (context.User.Identity?.IsAuthenticated == true)
+            {
+                var user = await userManager.GetUserAsync(context.User);
+
+                if (user is null)
+                {
+                    await signInManager.SignOutAsync();
+                    context.Response.Cookies.Delete("UserId");
+                    context.Response.Cookies.Delete("UserName");
+                    context.Response.Cookies.Delete("UserEmail");
+                    context.Response.Redirect("/Navigation/Home");
+                    return;
+                }
+                context.Items["CurrentUser"] = user;
+            }
+
+            await _next(context);
+        }
+    }
+}
