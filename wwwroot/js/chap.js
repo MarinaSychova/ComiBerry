5a1c64f675ff7cf1c3c7171f2cc903ae75181e58
MarinaSychova <139264690+MarinaSychova@users.noreply.github.com>
2025-07-27 12:43:43 +0300

    Add files via upload

---
+++ wwwroot/js/chap.js
@@ -0,0 +1,119 @@
+﻿function MoveUp(event) {
+	console.log("up");
+	const button = event.currentTarget;
+	console.log(button);
+	const page = button.closest('.page');
+	console.log(page);
+	const prevPage = page.previousElementSibling;
+	console.log(prevPage);
+	console.log("up");
+
+	if (prevPage && prevPage.classList.contains('page')) {
+		page.parentNode.insertBefore(page, prevPage);
+		updateOrders();
+		reindexPages();
+	}
+}
+
+function MoveDown(event) {
+	console.log("down");
+	const button = event.currentTarget;
+	const page = button.closest('.page');
+	const nextPage = page.nextElementSibling;
+
+	if (nextPage && nextPage.classList.contains('page')) {
+		page.parentNode.insertBefore(nextPage, page);
+		updateOrders();
+		reindexPages();
+	}
+}
+
+function DeletePage(event) {
+	const button = event.currentTarget;
+	const page = button.closest('.page');
+	page.remove();
+	reindexPages();
+}
+
+document.querySelectorAll('.move-up').forEach(button => {
+	button.addEventListener('click', MoveUp);
+});
+
+document.querySelectorAll('.move-down').forEach(button => {
+	button.addEventListener('click', MoveDown);
+});
+
+document.querySelectorAll('.delete').forEach(button => {
+	button.addEventListener('click', DeletePage);
+});
+
+function renderPreviews(preview) {
+	files.forEach(file => {
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			const div = document.createElement("div");
+			div.className = "preview-item";
+
+			const img = document.createElement("img");
+			img.src = e.target.result;
+			img.style.width = "100%";
+			img.style.objectFit = "cover";
+			img.style.aspectRatio = "1/1";
+
+			div.appendChild(img);
+			preview.appendChild(div);
+		};
+		reader.readAsDataURL(file);
+	});
+}
+
+function AddPageCommon(pageDiv, pageName) {
+	const buttonSpan = document.createElement("span");
+	pageDiv.appendChild(buttonSpan);
+
+	const moveUpButton = document.createElement("button");
+	moveUpButton.type = 'button';
+	moveUpButton.classList.add("move-up");
+	moveUpButton.classList.add("btn");
+	moveUpButton.classList.add("std-pad");
+	moveUpButton.innerHTML = "Up";
+	buttonSpan.appendChild(moveUpButton);
+	moveUpButton.addEventListener('click', MoveUp);
+
+	const moveDownButton = document.createElement("button");
+	moveDownButton.type = 'button';
+	moveDownButton.classList.add("move-down");
+	moveDownButton.classList.add("btn");
+	moveDownButton.classList.add("std-pad");
+	moveDownButton.innerHTML = "Down";
+	buttonSpan.appendChild(moveDownButton);
+	moveDownButton.addEventListener('click', MoveDown);
+
+	const imageSpan = document.createElement("span");
+	pageDiv.appendChild(imageSpan);
+
+	const imageInput = document.createElement("input");
+	imageInput.classList.add("btn");
+	imageInput.classList.add("std-pad");
+	imageInput.type = "file";
+	imageInput.name = pageName;
+	imageInput.accept = ".jpg, .png, .gif";
+	imageSpan.appendChild(imageInput);
+
+	const preview = document.createElement("div");
+	imageSpan.appendChild(preview);
+	imageInput.addEventListener("change", () => {
+		preview.innerHTML = "";
+		files = Array.from(imageInput.files);
+		renderPreviews(preview);
+	});
+
+	const deleteButton = document.createElement("button");
+	deleteButton.type = 'button';
+	deleteButton.classList.add("delete");
+	deleteButton.classList.add("btn");
+	deleteButton.classList.add("std-pad");
+	deleteButton.innerHTML = "Delete";
+	imageSpan.appendChild(deleteButton);
+	deleteButton.addEventListener('click', DeletePage);
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,136 @@
+﻿const smallScreen = window.matchMedia('(max-width: 1200px)');
+
+function handleScreenChange() {
+	const leftColumn = document.getElementById("left-column");
+	const middleColumn = document.getElementById("middle-column");
+	const rightColumn = document.getElementById("right-column");
+
+	if (smallScreen.matches) {
+		leftColumn.style.display = "none";
+		rightColumn.style.display = "none";
+
+		leftColumn.style.order = "0";
+		middleColumn.style.order = "1";
+		rightColumn.style.order = "0";
+
+		leftColumn.style.height = "55vh";
+		middleColumn.style.height = "55vh";
+		rightColumn.style.height = "55vh";
+	} else {
+		leftColumn.style.display = "block";
+		rightColumn.style.display = "block";
+
+		leftColumn.style.order = "0";
+		middleColumn.style.order = "0";
+		rightColumn.style.order = "0";
+
+		leftColumn.style.height = "70vh";
+		const pagePath = window.location.pathname;
+		const header = document.getElementsByTagName("header")[0];
+		const logo = document.getElementById("logo");
+		if (pagePath.startsWith("/Author/ReadChapter") || pagePath.startsWith("/Reader/ViewChapter"))
+		{
+			header.style.height = "9vh";
+			logo.style.height = "5vh";
+			middleColumn.style.height = "85vh";
+		}
+		else
+		{
+			header.style.height = "20vh";
+			logo.style.height = "15vh";
+			middleColumn.style.height = "70vh";
+		}
+		rightColumn.style.height = "70vh";
+	}
+}
+
+smallScreen.addListener(handleScreenChange);
+
+handleScreenChange();
+
+function OpenLogin() {
+	document.getElementById("left-column").style.display = "block";
+	document.getElementById("right-column").style.display = "none";
+
+	document.getElementById("login-button").style.display = "none";
+	document.getElementById("login-close-button").style.display = "block";
+
+	document.getElementById("search-button").style.display = "block";
+	document.getElementById("search-close-button").style.display = "none";
+}
+
+function CloseLogin() {
+	document.getElementById("left-column").style.display = "none";
+	document.getElementById("right-column").style.display = "none";
+
+	document.getElementById("login-button").style.display = "block";
+	document.getElementById("login-close-button").style.display = "none";
+
+	document.getElementById("search-button").style.display = "block";
+	document.getElementById("search-close-button").style.display = "none";
+}
+
+function OpenSearch() {
+	document.getElementById("left-column").style.display = "none";
+	document.getElementById("right-column").style.display = "block";
+
+	document.getElementById("login-button").style.display = "block";
+	document.getElementById("login-close-button").style.display = "none";
+
+	document.getElementById("search-button").style.display = "none";
+	document.getElementById("search-close-button").style.display = "block";
+}
+
+function CloseSearch() {
+	document.getElementById("left-column").style.display = "none";
+	document.getElementById("right-column").style.display = "none";
+
+	document.getElementById("login-button").style.display = "block";
+	document.getElementById("login-close-button").style.display = "none";
+
+	document.getElementById("search-button").style.display = "block";
+	document.getElementById("search-close-button").style.display = "none";
+}
+
+function GetMenu(IsLogged) {
+	const xhttp = new XMLHttpRequest();
+
+	xhttp.onload = function () {
+		document.getElementById("left-column").firstElementChild.innerHTML = xhttp.responseText;
+	}
+
+	let controller = "/Navigation/";
+	if (IsLogged) { controller += "MenuPartial"; }
+	else { controller += "LoginPartial"; }
+	xhttp.open("GET", controller, true);
+	xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	xhttp.send();
+}
+
+function GetAdminMenu() {
+	const xhttp = new XMLHttpRequest();
+	xhttp.onload = function () {
+		const divElement = document.createElement("div");
+		divElement.classList.add("tab");
+		divElement.classList.add("left");
+		divElement.classList.add("flex-column");
+		divElement.classList.add("std-pad-2");
+		const leftColumn = document.getElementById("left-column");
+		leftColumn.appendChild(divElement);
+		leftColumn.lastElementChild.innerHTML = xhttp.responseText;
+	}
+	xhttp.open("GET", "/Navigation/AdminMenuPartial", true);
+	xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	xhttp.send();
+}
+
+function GetSearch() {
+	const xhttp = new XMLHttpRequest();
+	xhttp.onload = function () {
+		const rightColumn = document.getElementById("right-column");
+		rightColumn.innerHTML += xhttp.responseText;
+	}
+	xhttp.open("GET", "/Navigation/SearchPartial", true);
+	xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+	xhttp.send();
+}
